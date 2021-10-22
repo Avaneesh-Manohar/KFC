3187a986d4e0ac8fdd7290d58213bd3b208259c0
Avaneesh-Manohar <91787960+Avaneesh-Manohar@users.noreply.github.com>
2021-10-22 18:15:07 +0530

    initialCommit

---
+++ script.js
@@ -0,0 +1,7 @@
+function show(){
+    // document.getElementById('over').style.top='0';
+    document.getElementById('over').style.display="block ";
+}
+function close(){
+    document.getElementById('over').style.display="none ";
+}
